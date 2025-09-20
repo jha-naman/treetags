99cf1045664a903a27742741db40b22f89ae5519
Naman <createnaman@gmail.com>
2025-09-20 11:22:16 +0530

    Add error handling for TagsConfiguration creation
    gracefully handles issues like syntax error's in the tags query

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
     - For tag file detection and opening
     - For UTF8 decoding during tag creation using treesitter queries
     - For errors raised during reading source files for creating tags
+    - For errors raised during creating treesitter tag configuration from a tag query
 
 ## [0.2.2] 2025-09-01
 
--- src/tags_config.rs
+++ src/tags_config.rs
@@ -1,6 +1,19 @@
 use tree_sitter::Language;
 use tree_sitter_tags::TagsConfiguration;
 
-pub fn get_tags_config(language: Language, tags_query: &str) -> TagsConfiguration {
-    TagsConfiguration::new(language, tags_query, "").unwrap()
+pub fn get_tags_config(
+    language: Language,
+    tags_query: &str,
+    lang_name: &str,
+) -> Result<TagsConfiguration, tree_sitter_tags::Error> {
+    match TagsConfiguration::new(language, tags_query, "") {
+        Ok(config) => Ok(config),
+        Err(e) => {
+            eprintln!(
+                "Error creating tags configuration for {} language: {}",
+                lang_name, e
+            );
+            Err(e)
+        }
+    }
 }
